635244568d0694f0abe0831f841eb3139bfc92ef
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-10-27 01:58:36 -0600

    connected cliet & server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './css/App.css';
+import axios from 'axios';
 
 function App() {
+
+  function APIdata() {
+    const data = {message: 'Active user details'};
+    axios.post("http://localhost:3001/hello", data)
+    .then(response => {
+      console.log(response.data)
+    })
+    console.log(data)
+  }
+
+  APIdata()
+
   return (
     <div className="App">
-     <input className= 'inputBar' placeholder='Enter your text here' type='text' />
+      <input className= 'inputBar' placeholder='Enter your text here' type='text' />
     </div>
   );
 }
+
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-app.use(cors()); 
 
+app.use(cors());
+app.use(express.json()); // Middleware to parse JSON data in the request body
 
-app.get('/hello', function(req, res) {
-   res.send("Welcome to the server page!");
+app.post('/hello', function(req, res) {
+   console.log("Received POST request");
+   console.log(req.body, req.params); // Access the JSON data sent in the request body
+   res.send('hi')
+   
 });
 
-app.listen(3001);
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
+});
