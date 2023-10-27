738a644f5e53dddc18bdca2a41dae59a45bffbd9
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-10-27 03:01:17 -0600

    temp storing search input in memory

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
 import './css/App.css';
 import axios from 'axios';
+import { useState } from 'react';
 
-function App() {
+
+const Cards = [
+  {id: 0, cImage: "ERW", setName:"ERE", number: "RE", rarity:"REER"},
+  {id: 1, cImage: "FDS", setName:"RE", number: "RE", rarity:"RW"},
+  {id: 2, cImage: "DFS", setName:"ERE", number: "RE", rarity:"ERWR3"}
+
+]
+
+
+export default function App() {
+  const [search, setSearch] = useState("");
 
   function APIdata() {
-    const data = {message: 'Active user details'};
+    const data = {message: search};
     axios.post("http://localhost:3001/hello", data)
-    .then(response => {
-      console.log(response.data)
-    })
-    console.log(data)
   }
+
+  function DisplayCards() {
+    const cardArr = Cards.map(card => 
+      <li className="Card" key={card.id}> {card.cImage} {card.number} </li>)
+    return <ul className='cardList'> {cardArr} </ul>
+  }
+
+  function handleClick(event: React.ChangeEvent<HTMLInputElement>) {
+    const inputValue = event.target.value;
+    setSearch(inputValue)
+    console.log("Someone is typing", inputValue)
+  }
+
 
   APIdata()
 
   return (
+    <>
     <div className="App">
-      <input className= 'inputBar' placeholder='Enter your text here' type='text' />
+      <input className= 'inputBar' onInput={handleClick} placeholder='Enter your text here' type='text' />
+
     </div>
+    <DisplayCards />
+
+  </>
   );
 }
 
 
-export default App;
