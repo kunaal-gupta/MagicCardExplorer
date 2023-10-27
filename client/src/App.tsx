43cc612ba4e7d36213687e70975a8891e077c83c
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-10-26 20:49:52 -0600

    added search bar & removed redundant files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <input className= 'inputBar' placeholder='Enter your text here' type='text' />
     </div>
   );
 }
